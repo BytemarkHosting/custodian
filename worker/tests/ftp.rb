c1ea9ce5c169193e254844448c4c83acf308314e
Steve Kemp <steve@steve.org.uk>
2012-11-13 11:08:29 +0000

    Updated to test.

--- worker/tests/ftp.rb
+++ worker/tests/ftp.rb
@@ -1,51 +1,124 @@
+#!/usr/bin/ruby 
+
+
 require 'timeout'
+require 'socket'
 
-
 #
-# Run an FTP test.
+# This class is responsible for performing tests against a remote FTP server
 #
 #
-# Return value
-#   TRUE:  The host is up
-#
-#  FALSE:  The host is not up
-#
-def ftp_test ( params )
+class FTPTest
 
   #
-  #  Get the hostname & port to test against.
+  # Data passed from the JSON hash.
   #
-  host = params['target_host']
-  port = params['test_port']
+  attr_reader :test_data
 
-  puts "FTP testing host #{host}:#{port}"
+  #
+  # The error text we return on failure.
+  #
+  attr_reader :error
 
 
-  begin
-    timeout(3) do
 
-      begin
-        socket = TCPSocket.new( host, port )
-        socket.puts( "QUIT")
 
-        banner = socket.gets(nil)
-        banner = banner[0,20]
+  #
+  # Save the data away.
+  #
+  def initialize( data )
+    @test_data = data
+  end
 
-        socket.close()
 
-        if ( banner =~ /^220/ )
-          puts "FTP alive: #{banner}"
-          return true
+  #
+  # Run the test.
+  #
+  #  Return "true" on success
+  #
+  #  Return "false" on failure.
+  #
+  # If the test fails the details should be retrieved from "get_details".
+  #
+  def run_test
+
+    #
+    #  Get the hostname & port to test against.
+    #
+    host = @test_data[:target_host]
+    port = @test_data[:test_port]
+
+    puts "FTP testing host #{host}:#{port}"
+
+    begin
+      timeout(3) do
+
+        begin
+          socket = TCPSocket.new( host, port )
+          socket.puts( "QUIT")
+
+          banner = socket.gets(nil)
+          banner = banner[0,20]
+
+          socket.close()
+
+          if ( banner =~ /^220/ )
+            return true
+          else
+            @error = "Banner didn't report OK: #{banner}"
+          end
+        rescue
+          @error = "FTP exception on host #{host}:#{port} - #{$!}"
+          return false
         end
-      rescue
-        puts "FTP exception on host #{host}:#{port} - #{$!}"
-        return false
       end
+    rescue Timeout::Error => e
+      @error = "Timed-out connecting #{e}"
+      return false
     end
-  rescue Timeout::Error => e
-    puts "TIMEOUT: #{e}"
+    @error = "Misc. failure."
     return false
   end
 
-  return false
+
+  #
+  #  Return the error.
+  #
+  def error
+    return @error
+  end
+
+end
+
+
+
+
+
+#
+# Sample test, for testing.
+#
+if __FILE__ == $0 then
+
+  #
+  #  Sample data.
+  #
+  test = {
+    :target_host => "mirror.bytemark.co.uk",
+    :test_type   => "ftp",
+    :test_port   => 21,
+    :test_alert  => "The FTP server no worky",
+  }
+
+
+  #
+  #  Run the test.
+  #
+  tst = FTPTest.new( test )
+  if ( tst.run_test )
+    puts "TEST OK"
+  else
+    puts "TEST FAILED"
+    puts tst.get_details()
+  end
+
 end
