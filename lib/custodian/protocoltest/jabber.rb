964d16d5f573b5a4c1571a1e1a52789f0cee565e
Steve Kemp <steve@steve.org.uk>
2012-11-21 21:37:57 +0000

    Added jabber protocol

---
+++ lib/custodian/protocoltest/jabber.rb
@@ -0,0 +1,89 @@
+#
+#  The Jabber-protocol test.
+#
+#  This object is instantiated if the parser sees a line such as:
+#
+###
+### foo.vm.bytemark.co.uk must run jabber otherwise 'chat failure'.
+###
+#
+#  The specification of the port is optional.
+#
+class JABBERTest < TCPTest
+
+
+  #
+  # The host to test against.
+  #
+  attr_reader :host
+
+
+  #
+  # The port to connect to.
+  #
+  attr_reader :port
+
+
+
+  #
+  # Constructor
+  #
+  def initialize( line )
+    #
+    # Save the host
+    #
+    @host  = line.split( /\s+/)[0]
+
+    #
+    # Save the port
+    #
+    if ( line =~ /on\s+([0-9]+)/ )
+      @port = $1.dup
+    else
+      @port = 5222
+    end
+  end
+
+
+
+
+  #
+  # Helper for development.
+  #
+  def to_s
+    "jabber-test of #{@host}:#{@port}."
+  end
+
+
+
+
+  #
+  # Run the TCP-protocol test.
+  #
+  def run_test
+
+    # reset the error, in case we were previously executed.
+    @error = nil
+
+    run_test_internal( @host, @port, "<stream:stream" )
+  end
+
+
+
+
+  #
+  # If the test fails then report the error.
+  #
+  def error
+    @error
+  end
+
+
+
+
+  register_test_type "jabber"
+
+
+
+
+end
