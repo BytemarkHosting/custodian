ea4e48e4833fbb8eb2b52f3f9263a989b87a0a1f
Steve Kemp <steve@steve.org.uk>
2012-11-21 16:41:44 +0000

    Added stub for class-factory + test objects.

---
+++ lib/custodian/protocoltest.rb
@@ -0,0 +1,66 @@
+
+#
+#
+# Base class for custodian protocol-tests
+#
+# Each subclass will register themselves, via the call
+# to 'register_test_type'.
+#
+# This class is a factory that will return the correct
+# derived class for a given line from our configuration
+# file.
+#
+#
+class ProtocolTest
+
+
+  #
+  # The subclasses we have.
+  #
+  @@subclasses = { }
+
+
+  #
+  # Create a test-type object given a line of text from our parser.
+  #
+  # The line will be like "target must run tcp|ssh|ftp|smtp .."
+  #
+  #
+  def self.create( line )
+    if ( line =~ /must\s+run\s+(\S+)(\s+|\.|$)/ )
+      test_type = $1.dup
+      test_type.chomp!( "." )
+
+      c = @@subclasses[test_type]
+      if c
+        c.new( line )
+      else
+        raise ArgumentError, "Bad test type: #{test_type}"
+      end
+    else
+      raise "Uknown line given"
+    end
+  end
+
+
+  #
+  # Register a new test type - this must be called by our derived classes
+  #
+  def self.register_test_type name
+    @@subclasses[name] = self
+  end
+
+
+  #
+  # Return an array of test-types we know about
+  #
+  # i.e. Derived classes that have registered themselves.
+  #
+  #
+  def types
+    @@subclasses
+  end
+
+
+end
+
