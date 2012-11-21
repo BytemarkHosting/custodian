628329a3028b55e1cfb3bdf1345b3ecf9ad8fe45
Steve Kemp <steve@steve.org.uk>
2012-11-21 21:15:45 +0000

    Added rsync test.

---
+++ lib/custodian/protocoltest/rsync.rb
@@ -0,0 +1,90 @@
+#
+#  The RSYNC-protocol test.
+#
+#  This object is instantiated if the parser sees a line such as:
+#
+###
+### upload.ns.bytemark.co.uk must run rsync otherwise 'dns upload failure'.
+###
+#
+#  The specification of the port is optional and defaults to 873
+#
+class RSYNCTest < TCPTest
+
+
+  #
+  # The host to test against.
+  #
+  attr_reader :host
+
+
+  #
+  # The port to connect to.
+  #
+  attr_reader :port
+
+
+
+
+  #
+  # Constructor
+  #
+  def initialize( line )
+    #
+    # Save the host
+    #
+    @host  = line.split( /\s+/)[0]
+
+    #
+    # Save the port
+    #
+    if ( line =~ /on\s+([0-9]+)/ )
+      @port = $1.dup
+    else
+      @port = 873
+    end
+  end
+
+
+
+
+  #
+  # Helper for development.
+  #
+  def to_s
+    "rsync-test of #{@host}:#{@port}."
+  end
+
+
+
+
+  #
+  # Run the protocol test.
+  #
+  def run_test
+
+    # reset the error, in case we were previously executed.
+    @error = nil
+
+    run_test_internal( @host, @port, "@RSYNCD" )
+  end
+
+
+
+
+  #
+  # If the test fails then report the error.
+  #
+  def error
+    @error
+  end
+
+
+
+
+  register_test_type "rsync"
+
+
+
+
+end
