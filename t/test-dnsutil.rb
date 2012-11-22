3ac252151c374e067425dd7efe355bd76ba122d1
Steve Kemp <steve@steve.org.uk>
2012-11-22 06:44:44 +0000

    Updated for new name.

--- t/test-dnsutil.rb
+++ t/test-dnsutil.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby1.8 -I./lib/ -I../lib/
 
 
-require 'custodian/dnsutil'
+require 'custodian/util/dns'
 require 'test/unit'
 
 
@@ -33,19 +33,19 @@ class TestDNSUtil < Test::Unit::TestCase
     #
     # IPv6 lookup
     #
-    details = DNSUtil.hostname_to_ip( "ipv6.steve.org.uk" )
+    details = Custodian::Util::DNS.hostname_to_ip( "ipv6.steve.org.uk" )
     assert( details =~ /2001:41c8:125:46::10/i )
 
     #
     # IPv4 lookup
     #
-    details = DNSUtil.hostname_to_ip( "ipv4.steve.org.uk" )
+    details = Custodian::Util::DNS.hostname_to_ip( "ipv4.steve.org.uk" )
     assert( details =~ /80.68.85.46/i )
 
     #
     # Failure case
     #
-    details = DNSUtil.hostname_to_ip( "this.doesnot.exist" )
+    details = Custodian::Util::DNS.hostname_to_ip( "this.doesnot.exist" )
     assert( details.nil? )
 
   end
@@ -59,19 +59,19 @@ class TestDNSUtil < Test::Unit::TestCase
     #
     # IPv6 lookup
     #
-    details = DNSUtil.ip_to_hostname( "2001:41c8:125:46::22" )
+    details = Custodian::Util::DNS.ip_to_hostname( "2001:41c8:125:46::22" )
     assert( details =~ /ssh.steve.org.uk/i )
 
     #
     # IPv4 lookup
     #
-    details = DNSUtil.ip_to_hostname( "80.68.85.48" )
+    details = Custodian::Util::DNS.ip_to_hostname( "80.68.85.48" )
     assert( details =~ /ssh.steve.org.uk/i )
 
     #
     # Bogus lookup - should return nil.
     #
-    details = DNSUtil.ip_to_hostname( "800.683.853.348" )
+    details = Custodian::Util::DNS.ip_to_hostname( "800.683.853.348" )
     assert( details.nil? )
   end
 
