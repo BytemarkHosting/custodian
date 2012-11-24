9a418822f1af2d1786c07a040bdcf3de4c69f199
Steve Kemp <steve@steve.org.uk>
2012-11-24 12:36:13 +0000

    The single file which includes all known protocol tests.

---
+++ lib/custodian/protocoltest.rb
@@ -0,0 +1,28 @@
+#
+# This is just a helper which means that you'll only need to
+# update the list of requires in one-place if you add a new
+# protocol test-type
+#
+
+
+
+#
+# The individual protocol tests.
+#
+require 'custodian/protocoltest/tcp'
+require 'custodian/protocoltest/dns'
+require 'custodian/protocoltest/ftp'
+require 'custodian/protocoltest/http'
+require 'custodian/protocoltest/jabber'
+require 'custodian/protocoltest/ldap'
+require 'custodian/protocoltest/ping'
+require 'custodian/protocoltest/pop3'
+require 'custodian/protocoltest/rsync'
+require 'custodian/protocoltest/ssh'
+require 'custodian/protocoltest/smtp'
+
+
+#
+# The factory
+#
+require 'custodian/testfactory'
