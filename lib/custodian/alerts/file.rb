278345193b1dc96c31528882e7bb1b4fc160fcb2
Steve Kemp <steve@steve.org.uk>
2012-11-24 16:25:11 +0000

    Added new alert-type, to merely write to a file.

---
+++ lib/custodian/alerts/file.rb
@@ -0,0 +1,66 @@
+
+
+
+#
+#  The SMTP-alerter.
+#
+module Custodian
+
+  module Alerter
+
+    class AlertFile < AlertFactory
+
+      #
+      # The test this alerter cares about
+      #
+      attr_reader :test
+
+
+
+      #
+      # Constructor - save the test-object away.
+      #
+      def initialize( obj )
+        @test = obj
+      end
+
+
+
+      #
+      # Record a raise event for the given test.
+      #
+      def raise
+        write_message( "RAISE: #{test.target} failed #{test.get_type}-test - #{test.error()}" )
+      end
+
+
+
+      #
+      # Record a clear event for the given test.
+      #
+      def clear
+        write_message( "CLEAR: #{test.target} failed #{test.get_type}-test" )
+      end
+
+
+      #
+      # Write the actual message to our target.
+      #
+      def write_message( msg )
+        file = File.open(@target, "a")
+        file.puts( "#{Time.now} #{msg}" )
+        file.close
+
+      end
+
+
+
+
+      register_alert_type "file"
+
+
+
+
+    end
+  end
+end
