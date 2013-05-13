b7a397585ac1cda99356496a301c6c46833b126d
Steve Kemp <steve@steve.org.uk>
2013-05-13 16:57:15 +0100

    We don't read from the socket for jabber-protocol tests.

--- lib/custodian/protocoltest/jabber.rb
+++ lib/custodian/protocoltest/jabber.rb
@@ -74,7 +74,7 @@ module Custodian
         # reset the error, in case we were previously executed.
         @error = nil
 
-        run_test_internal( @host, @port )
+        run_test_internal( @host, @port, nil, false )
       end
 
 
