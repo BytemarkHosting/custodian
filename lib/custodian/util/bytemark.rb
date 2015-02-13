ec243bb811313ac38fec1ac7ee4e3eb33b92834f
Steve Kemp <steve@steve.org.uk>
2015-02-13 09:20:20 +0000

    Added 5.28.58.0/24 to the Bytemark ranges.

--- lib/custodian/util/bytemark.rb
+++ lib/custodian/util/bytemark.rb
@@ -20,7 +20,7 @@ module Custodian
       # These are used to test if an alert refers to a machine outwith our
       # network.
       #
-      BYTEMARK_RANGES = %w(80.68.80.0/20 89.16.160.0/19 212.110.160.0/19 46.43.0.0/18 91.223.58.0/24 213.138.96.0/19 5.153.224.0/21 2001:41c8::/29 2001:41c9::/32).collect{|i| IPAddr.new(i)}
+      BYTEMARK_RANGES = %w(80.68.80.0/20 89.16.160.0/19 212.110.160.0/19 46.43.0.0/18 91.223.58.0/24 213.138.96.0/19 5.153.224.0/21 5.28.58.0/24 2001:41c8::/29 2001:41c9::/32).collect{|i| IPAddr.new(i)}
 
 
       #
