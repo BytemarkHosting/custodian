f150b7ea0951fc92c4f92b7ef890c7ee305cc82c
Steve Kemp <steve@steve.org.uk>
2015-02-17 20:50:38 +0000

    Updated for new test-type API.
    Now that the class-factory returns an Array, we've updated the
    test-cases to continue to pass.

--- t/test-ldap-probe.rb
+++ t/test-ldap-probe.rb
@@ -32,8 +32,9 @@ class TestLDAPProbe < Test::Unit::TestCase
      test = Custodian::TestFactory.create( "auth.bytemark.co.uk must run ldap on 389 with username 'testing' with password 'bob' otherwise 'LDAP dead?'." )
     end
 
-    assert( test )
-    assert_equal( test.get_type, "ldap" )
+    assert( test.kind_of? Array )
+    assert( ! test.empty? )
+    assert_equal( test[0].get_type, "ldap" )
   end
 
 
@@ -57,6 +58,10 @@ class TestLDAPProbe < Test::Unit::TestCase
     data.each do |str|
       assert_raise ArgumentError do
         test = Custodian::TestFactory.create( str )
+
+        assert( test.kind_of? Array )
+        assert( ! test.empty? )
+
       end
     end
   end
